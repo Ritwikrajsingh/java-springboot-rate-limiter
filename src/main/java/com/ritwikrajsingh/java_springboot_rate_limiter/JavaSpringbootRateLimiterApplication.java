331ba75c49b29095caaf07705f4365dcb758898e
ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-03-11 14:56:44 +0530

    feat: spring init

---
+++ src/main/java/com/ritwikrajsingh/java_springboot_rate_limiter/JavaSpringbootRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.ritwikrajsingh.java_springboot_rate_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootRateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ritwikrajsingh/java_springboot_rate_limiter/JavaSpringbootRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritwikrajsingh.java_springboot_rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootRateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
